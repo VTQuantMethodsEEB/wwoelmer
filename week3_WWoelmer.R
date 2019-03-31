ad25ebc99879550c8633320221cd6bba397e194d
klangwig <klangwig@gmail.com>
2019-03-31 09:29:09 -0400

    see comments on week 3 and in README

--- week3_WWoelmer.R
+++ week3_WWoelmer.R
@@ -9,7 +9,9 @@ bvr$Date <- as.Date(bvr$Date, format = "%Y-%m-%d")
 unique(bvr$Date)
 
 # plot changes in temperature over time based on depth
-ggplot(data = bvr, aes(x =Date, y = Temp_C , color = Depth_m) ) + geom_point()
+ggplot(data = bvr, aes(x =Date, y = Temp_C , color = Depth_m) ) + 
+  geom_point()+
+  scale_colour_gradient(trans = "reverse")##added this here
 ### I want the colors to be opposite here, so the high depths (deeper) are the darker colors
 
 # now the same idea but with chlorophyll a
@@ -21,8 +23,17 @@ ggplot(data = bvr, aes(x = Date, y = Chla_ugL, color= Depth_m)) + geom_point()
 bvr$year <- format(as.Date(bvr$Date, format="%d/%m/%Y"),"%Y")
 bvr$mo_day <- format(as.Date(bvr$Date, format="%d/%m/%Y"),"%m-%d")
 bvr$mo_day <- as.Date(bvr$mo_day, format = "%m-%d")
+#lubridate can be a better option for working with dates
+library(lubridate)
+#https://lubridate.tidyverse.org/
+
+bvr$mo <- month(bvr$Date)
+bvr$day <- day(bvr$Date)
+#you could paste them together, or do some reading on lubridate
+
 # any idea why this adds the '2019' onto the month-day? I haven't figured out a way to get it to
 # stay month-day without the year being added
+--### see above###---
 
 ggplot(data = bvr, aes(x = mo_day, y = Chla_ugL, color = year)) + geom_point() 
 # this plot is cool but doesn't show the differences as you go deeper into the reservoir
@@ -30,3 +41,11 @@ ggplot(data = bvr, aes(x = mo_day, y = Chla_ugL, color = year)) + geom_point()
 ggplot(data = bvr, aes(x = mo_day, y = Chla_ugL, color = year)) + geom_point() + facet_wrap(~Depth_m)
 # is there a way to select only some depths to plot, e.g., I want to make facet plots of just 
 # 0.1, 1, 5, 8, and 12
+
+ggplot(data = subset(bvr, Depth_m == 0.1| Depth_m== 1 | Depth_m == 5), aes(x = mo_day, y = Chla_ugL, color = year)) + 
+         geom_point() + 
+         facet_wrap(~Depth_m)
+# is there a way to select only some depths to plot, e.g., I want to make facet plots of just 
+# 0.1, 1, 5, 8, and 12
+
+###you can add the subset command into ggplot as above
