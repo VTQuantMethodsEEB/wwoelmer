14cb3dc47ee0fc533a2f9ca45b8a16f77f0818ee
wwoelmer <wwoelmer@vt.edu>
2019-04-23 14:55:21 -0400

    file upload

---
+++ FinalProject_WWoelmer.R
@@ -0,0 +1,25 @@
+#Your final project is due on Monday May 13 by midnight. 
+#The final project will be to discuss and perform a group of statistical analyses. 
+#Both the scientific and statistical background should be clearly presented, and future statistical 
+#steps you plan to take. These should be written and presented like the results section of a 
+#scientific paper, with a full ABSTRACT, very brief introduction with a background/hypotheses, methods, 
+#and discussion section sufficient for understanding the results. 
+#It is acceptable to use the previously written paragraph about your data as background.
+#Bullet points in the introduction, methods, and discussion section are acceptable. 
+#You should think of this an outline of  a thesis paper. Usually we flesh out the results and the figures, 
+#then add the rest of the stuff. I write my abstracts as a mini-outline that helps me figure out what 
+#should go in each section.
+#A few notes: 
+#  It is perfectly fine to use a paper you are already writing, but nothing that has already been 
+#sent out for peer-review please. 
+# Our last day (Tuesday May 7) we will do peer review on projects.
+#Title, Name and authors
+#Abstract (full, ~150 – 300 words, think about journals!) 
+#Introduction (background, hypotheses; bulleted is okay)
+#Methods (scientific and statistical; bulleted is okay)
+#Results (fully written like a scientific paper)
+#Please put figures in this section of paper with their captions directly below them.
+#Include statistical results as 1) either in the text, 2) in main text tables, or 3) as supplemental 
+#tables as you would in a real paper (more on this later).
+#Discussion (what do your results mean, future statistical analyses to be conducted, 
+#what this tells us about the world; bulleted is okay)
